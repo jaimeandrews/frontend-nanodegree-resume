d168edbdd23c43840d55e716d8e064d8f0ae92d4
Jaime Andrews <jaime.andrews@gmail.com>
2015-02-16 19:02:38 -0300

    bug 1
    No aparece nada en pantalla

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
 $("#main").append("Jaime Andrews");
+var name = "Jaime Andrews";
+var role = "Ingeniero en Informatica";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%"), role );
+$("#header").prepend(formattedName, formattedRole);
