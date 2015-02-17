bbf588acd10b8928fbb6c00bf4f5fca98ec4255c
Jaime Andrews <jaime.andrews@gmail.com>
2015-02-17 17:49:00 -0300

    bug1 reparado
    Un paréntesis de mas

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,7 @@
-$("#main").append("Jaime Andrews");
-var name = "Jaime Andrews";
+//$("#main").append("Jaime Andrews");
+var name = "Jaime Andrews Soto";
 var role = "Ingeniero en Informatica";
 var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%"), role );
-$("#header").prepend(formattedName, formattedRole);
+var formattedRole = HTMLheaderRole.replace("%data%", role );
+$("#header").prepend(formattedName,formattedRole);
+	
