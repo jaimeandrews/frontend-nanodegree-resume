acb48bd0620b444b35a63c724d989289aedadc8c
Jaime Andrews <jaime.andrews@gmail.com>
2015-02-18 00:38:33 -0300

    Skill en la pantalla

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,16 +10,67 @@ var bio ={"name" :"Jaime Andrews","role" : "Ingeniero en Informatica" ,
 	},
 	"welcomeMessage":"Welcome to my page",
 	"pictureUrl" :"https://gp3.googleusercontent.com/-5dheV2IgTdU/AAAAAAAAAAI/AAAAAAAAAAA/qXGsbZnjgEI/s48-c-k-no/photo.jpg" , 
-	"skills" :["Developer", "Python", "JS", "C++"]	};
-//var name = "Jaime Andrews Soto";
-//var role = "Ingeniero en Informatica";
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedmail = HTMLemail.replace("%data%", bio.contacts.mail)
-var formattedskills = HTMLskills.replace("%data%", bio.skills);
-var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contacto);
-var formattedPictureUrl = HTMLbioPic.replace("%data%", bio.pictureUrl);
+	"skills" :["Developer", "Python", "JS", "C++"]	}
+var work ={ 
+	"jobs":[
+	{
+		"employer": "Xerox de Chile",
+		"tittle":"Logistic planner", 
+		"dates": "December 2009- December 2014", "Description": "Santiago"
+	},
 
-$("#header").prepend(formattedName,formattedRole);
-$("#main").append(formattedskills,formattedContact,formattedPictureUrl, formattedmail)
+	{
+		"employer": "Xerox de Chile",
+		"tittle":"Field Engineer", 
+		"dates": "November 1997- December 2009", "Description": "RT"
+	},
 
+	{
+		"employer": "Fuerza Aerea de Chile",
+		"tittle":" Electronics Technical", 
+		"dates": "January 1994- December 1997", "Description": "Santiago"
+	}
+	]}
+
+var education = { 
+	"schools" :[
+	{
+		"name": "Universidad Andrés Bello", 
+		"ciudad":"Santiago",
+		"titulo":"Ingeniero en Computación e Informática",
+		"Año de Egreso": "2006"
+	},
+	{
+		"name": "Escuela de Especialidades Fuerza Aerea de Chile", 
+		"ciudad":"Santiago",
+		"titulo":"Mecánico Electrónico de Aeronaves",
+		"Año de Egreso": "1993"}
+	]
+}
+
+var projects ={
+	"projects":[
+		{
+			"tittle":"Sistema de Ingreso pacientes podología",
+			"dates":"Febrero de 2015",
+			"description": "Sistema de ingreso de pacientes de podología",
+			"images": [ "url1","url2","url3"]
+		}
+
+	]
+}
+var formattedname = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedname);
+var formattedrole =HTMLheaderRole.replace("%data%", bio.role);
+$("#header").append(formattedrole);
+if (bio.skills.length >0){
+	$("#header").append(HTMLskillsStart);
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+	$("#skills").append(formattedSkill);
+}
